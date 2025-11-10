25b83a723e07b7cb055ef4114d88ce2d50009553
jeanne-lcr <jeanne.leclere@efrei.net>
2025-11-10 16:51:02 +0100

    3.1

--- tarjan.c
+++ tarjan.c
@@ -12,3 +12,12 @@ t_tarjan_vertex* arrayVertices(AdjacencyList adj){
     }
     return arr;
 }//don't forget to free the memory after using this function !!
+
+void parcours (AdjacencyList adj, t_tarjan_vertex vertex, t_partition *partition,t_stack *stack, t_class *class) {
+    vertex=arrayVertices(adj);
+    stack->nbElmts=0;
+    partition->classes=NULL;
+    partition->size;
+    partition->capacity=0;
+    int index = 0;
+    int classCounter = 0;
