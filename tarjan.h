9d12a2b0df2c586414b146d65a00bbf479cdc98f
jeanne-lcr <jeanne.leclere@efrei.net>
2025-11-10 14:24:05 +0100

    tarjan

---
+++ tarjan.h
@@ -0,0 +1,14 @@
+//
+// Created by jeann on 10/11/2025.
+//
+
+#ifndef TARJAN_H
+#define TARJAN_H
+typedef struct tarjan{
+  int identifier;
+  int number;
+  int access;
+  int bool;
+}t_tarjan_vertex;
+
+#endif //TARJAN_H
